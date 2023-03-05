ba934ff20c8cbfa2f48561ddee7c827cf713cb35
yusif19 <eliyevy967@gmail.com>
2023-03-05 21:15:00 +0400

    initial commit

---
+++ hometask77/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace hometask77
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            int increment = 0;
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] numbers = new int[size];
+            do
+            {
+
+                Console.WriteLine("enter element");
+                int element = Convert.ToInt32(Console.ReadLine());
+
+
+                Array.Resize(ref numbers, increment + 1);
+
+                numbers[increment] = element;
+                increment++;
+                Console.WriteLine("Do you want do continue?");
+                string answer = Console.ReadLine();
+                if (!(answer == "y" || answer == "Y"))
+                {
+                    flag = false;
+
+                }
+            }
+            while (flag);
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+
+            }
+        }
+    }
+}
